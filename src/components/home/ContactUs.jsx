5cc684b051051c1688865e29c171799fe61ac805
Ahsn@1234 <ahmad507eb@gmail.com>
2023-10-24 21:55:29 +0500

    commit with add contents

--- src/components/home/ContactUs.jsx
+++ src/components/home/ContactUs.jsx
@@ -3,14 +3,14 @@ import { Element } from 'react-scroll'
 function ContactUs() {
   return (
     <Element name="contact">
-    <section className="bg-gradient-to-r from-white to-gray-100 h-full">
-      <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-        <h2 className="text-center mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+    <section className="h-full bg-gradient-to-r from-white to-gray-100">
+      <div className="max-w-screen-md px-4 py-8 mx-auto lg:py-16">
+        <h2 className="mb-4 text-4xl font-extrabold tracking-tight text-center text-gray-900 dark:text-white">
           Contact Us
         </h2>
-        <div className="h-1 mx-auto bg-primary-300 w-24 opacity-75 mt-4 rounded"></div>
-        <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-          Got a technical issue? Want to get any help about anything feel free to contact us and get any help about anything? <strong className='font-bold'>Call at: <span className='text-black'>03186595507</span></strong> and also <strong className='font-bold'> Email us: <span className='text-black'>ahsanzjt@gmail.com</span> </strong>
+        <div className="w-24 h-1 mx-auto mt-4 rounded opacity-75 bg-primary-300"></div>
+        <p className="mb-8 font-light text-center text-gray-500 lg:mb-16 dark:text-gray-400 sm:text-xl">
+          Got a technical issue? Want to get any help about anything feel free to get any help about anything? <strong className='font-bold'>Call at: <span className='text-black'>03186595507</span></strong> and also <strong className='font-bold'> Email us: <span className='text-black'>ahsanzjt@gmail.com</span> </strong>
         </p>
         <form action="#" className="space-y-8">
           <div>
@@ -50,7 +50,7 @@ function ContactUs() {
           </div>
           <button
             type="submit"
-            className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-600 sm:w-fit hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            className="px-5 py-3 text-sm font-medium text-center text-white rounded-lg bg-primary-600 sm:w-fit hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
           >
             Send message
           </button>
